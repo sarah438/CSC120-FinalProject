e210abe2bb0cfe2d021b163642bdf02fe824d416
nmohamed54321 <123846987+nmohamed54321@users.noreply.github.com>
2023-05-02 19:47:39 -0400

    Update Schedule.java

--- Schedule.java
+++ Schedule.java
@@ -4,6 +4,10 @@ public class Schedule {
     Major cs_major = new Major();
 
     public void recommend(){
+        int x = 0;
+        while (!cantake()){
+
+        }
         
     }
 
@@ -16,17 +20,30 @@ public class Schedule {
         String course_name;
         int num_credits;
         String prof;
+        ArrayList<Course> prevCoursesPreReqs = new ArrayList<>();
+        
+            System
 
-        for(int i = 0; i < numCourses; i++){
-            System.out.print("Enter course name: ");
-            course_name = input.nextLine();
-            System.out.print("Enter number of credits: ");
-            num_credits = input.nextInt();
-            System.out.print("Enter the professor name: ");
-            prof = input.nextLine();
-
-            
-            //prevCourses.add
+        // figure out a way to print the major in a numbered list and have the user choose which classes they've by say 1, 2, etc.@interface
+        // for(int i = 0; i < numCourses; i++){
+        //     System.out.print("Enter course name: ");
+        //     course_name = input.nextLine();
+        //     System.out.print("Enter number of credits: ");
+        //     num_credits = input.nextInt();
+        //     System.out.print("Enter the professor name: ");
+        //     prof = input.nextLine();
+        //     System.out.print("How many prerequisite courses does this class have? ");
+        //     int num = input.nextInt();
+        //     ArrayList<Course> empty = new ArrayList<>();
+        //     for (int j = 0; j < num; j++){
+        //         System.out.print("Enter prerequisite course: ");
+        //         String preCourse = input.nextLine();
+        //         System.out.print("Enter the number of credits: ");
+        //         int numC = input.nextInt();
+        //         System.out.print("Enter the professor name: ");
+        //         String profName = input.nextLine();
+        //     Course course = new Course(course_name, num_credits, prof, prevCoursesPreReqs);
+            }
         }
     }
 }
