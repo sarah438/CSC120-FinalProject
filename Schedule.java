64442ba9c28baa2e261bae6eda1bbda9197f51f3
sarah438 <124069595+sarah438@users.noreply.github.com>
2023-05-04 22:05:53 -0400

    Update Schedule.java

--- Schedule.java
+++ Schedule.java
@@ -1,34 +1,49 @@
+import java.util.Scanner;
 import java.util.ArrayList;
-public class Course {
-    private String title;
-    private int credit;
-    private String professor;
-    private ArrayList<Course> prereqs;
+public class Schedule {
+    Major cs_major = new Major();
 
+// add javadic for all methods    
 
-//Add javadoc explaining what certain parts of the code does.
-
-   // ArrayList<Course> prereqs;
-    public Course(String title, int n_credits, String professor, ArrayList<Course> givenpreqs){
-        this.title= title;
-        this.credit= n_credits;
-        this.professor= professor;
-        //this.prereqs= givenpreqs;
-        for (Course c: givenpreqs) {
-            this.prereqs.add(c);
+    public void recommend(){
+        int x = 0;
+        while (!cs_major.cantake()){
+            System.out.print("hi");
         }
-
-    }
-    public boolean cantake(ArrayList<Course> alreadytaken){
-        return alreadytaken.containsAll(this.prereqs);
-
-    }
-    public boolean isprereqs(Course c){
-        return this.prereqs.contains(c);
+        
     }
 
+    public static void main(String[] args) {
+        System.out.print("hi");
+        Course newCourse;
+        Scanner input = new Scanner(System.in);
+        ArrayList<Course> prevCourses = new ArrayList<>();
+        System.out.println("Hi. How many courses have you taken so far?");
+        int numCourses = input.nextInt();
+        String course_name;
+        int num_credits;
+        String prof;
+        ArrayList<Course> prevCoursesPreReqs = new ArrayList<>();
 
-    
-
+        // figure out a way to print the major in a numbered list and have the user choose which classes they've by say 1, 2, etc.@interface
+        // for(int i = 0; i < numCourses; i++){
+        //     System.out.print("Enter course name: ");
+        //     course_name = input.nextLine();
+        //     System.out.print("Enter number of credits: ");
+        //     num_credits = input.nextInt();
+        //     System.out.print("Enter the professor name: ");
+        //     prof = input.nextLine();
+        //     System.out.print("How many prerequisite courses does this class have? ");
+        //     int num = input.nextInt();
+        //     ArrayList<Course> empty = new ArrayList<>();
+        //     for (int j = 0; j < num; j++){
+        //         System.out.print("Enter prerequisite course: ");
+        //         String preCourse = input.nextLine();
+        //         System.out.print("Enter the number of credits: ");
+        //         int numC = input.nextInt();
+        //         System.out.print("Enter the professor name: ");
+        //         String profName = input.nextLine();
+        //     Course course = new Course(course_name, num_credits, prof, prevCoursesPreReqs);
+            }
 }
-
+    
