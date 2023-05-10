60e622e77905d207c2d5773ead8ef785ef26f313
nmohamed54321 <123846987+nmohamed54321@users.noreply.github.com>
2023-05-10 12:14:39 -0400

    Delete CourseComparator.java

--- CourseComparator.java
+++
@@ -1,8 +0,0 @@
-import java.util.Comparator;
-public class CourseComparator implements Comparator<Course>{
-    @Override
-    public int compare(Course c1, Course c2) {
-        return c1.getTitle().compareTo(c2.getTitle());
-    }
-}
-
