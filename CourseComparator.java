7891e787a870e3e4fcb070cfa6748ca3d3a68b89
sarah438 <124069595+sarah438@users.noreply.github.com>
2023-05-12 08:44:25 -0400

    Update CourseComparator.java

--- CourseComparator.java
+++ CourseComparator.java
@@ -4,9 +4,10 @@ public class CourseComparator implements Comparator<Course>{
     /*
      * This function compares our two parameters.getTitle 
      * Hence the main thing that this method is doing is to compare the two parameters and then sort them out later when it's called in the major class.
-     * @Param Course c1
-     * @Param Course cs
-     * returns the int in a sorted out way.
+     * Returns a sorted hashtable
+     * @param Course c1
+     * @param Course cs
+     * @return c1.getTitle().compareTo(c2.getTitle())
      */
     public int compare(Course c1, Course c2) {
         return c1.getTitle().compareTo(c2.getTitle());
